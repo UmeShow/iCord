58df65890fc2e32857f0a3a8ebd3363ac35823b6
UmeShow <umeshow@MacBook-Air.local>
2025-10-16 21:09:04 +0900

    Feat: Improve UI and add name input field

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: {
-    // ワークスペースのルートを明示的に指定
-    outputFileTracingRoot: __dirname,
-  },
   // 画像ドメインの設定
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  // 出力設定
+  output: 'standalone'
 }
 
 module.exports = nextConfig
